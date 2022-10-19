e2c41b936be251da1b095655ee57037cc22e0124
Maria Sigmon <mariars2002@yahoo.com>
2022-10-19 16:59:17 -0400

    Add sumr.c

---
+++ 02_FRAMES/sumr/sumr.c
@@ -0,0 +1,27 @@
+//Compute sum of consecutive integers from 1 to N (inclusive)
+//git will track everything else
+#include <stdio.h>
+
+long cumulative_sum(int N);
+
+int main(int argc, char *argv[])
+{
+	//Ask for number
+	int N;
+	long result;
+
+	printf("Enter a number:\n");
+	scanf("%d", &N); //%d
+	//Computer number
+	result = cumulative_sum(N);
+	//Retunr number
+	printf("The sum from 1 to %d (inclusive) is %ld\n", N, result);
+	return 0;
+}
+
+long cumulative_sum(int N)
+{
+//TODO
+
+}
+
