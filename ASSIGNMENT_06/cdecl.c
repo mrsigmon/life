0f3af6a6cd86fb53ddf596150551cb1a0596355d
Maria Sigmon <mariars2002@yahoo.com>
2022-10-25 14:31:17 -0400

    Fix indentation

--- ASSIGNMENT_06/cdecl.c
+++ ASSIGNMENT_06/cdecl.c
@@ -93,7 +93,9 @@ deal_with_function_args() {
     }
     gettoken();
    printf("function returning ");
-}deal_with_pointers() {
+}
+
+deal_with_pointers() {
    while ( stack[top].type== '*' ) {
         printf("%s ", pop.string );
    }
