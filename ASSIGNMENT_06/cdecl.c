12753a74aee4e3b65ad98cc0c5dee2daab19b30a
Maria Sigmon <mariars2002@yahoo.com>
2022-10-25 15:07:32 -0400

    Fix brace error

--- ASSIGNMENT_06/cdecl.c
+++ ASSIGNMENT_06/cdecl.c
@@ -99,6 +99,7 @@ deal_with_pointers() {
    while ( stack[top].type== '*' ) {
         printf("%s ", pop.string );
    }
+}
 deal_with_declarator() {
    /* deal with possible array/function following the identifier */
    switch (this.type) {
@@ -116,4 +117,4 @@ deal_with_pointers();
             printf("%s ",pop.string);
         }
    }
-}}
+}
