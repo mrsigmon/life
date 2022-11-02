9ef855ea75f50ed9bca4397249ad2bcf00b2a334
Maria Sigmon <mariars2002@yahoo.com>
2022-11-02 17:47:51 -0400

    Add 03_MODULES

---
+++ 03_MODULES/sumh/main.c
@@ -0,0 +1,21 @@
+//Compute sum of consecutive integers from 1 to N (inclusive)
+//git will track everything else
+#include <stdio.h>
+#include "mylib.h"
+
+int main(int argc, char *argv[])
+{
+	//Ask for number
+	int N;
+	long result;
+
+	printf("Enter a number:\n");
+	scanf("%d", &N); //%d
+	//Computer number
+	result = cumulative_sum(N);
+	//Retunr number
+	printf("The sum from 1 to %d (inclusive) is %ld\n", N, result);
+	return 0;
+}
+
+
