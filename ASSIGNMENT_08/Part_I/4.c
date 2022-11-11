063adec11af449d5a91f5b50043593bc721c3bf7
Maria Sigmon <mariars2002@yahoo.com>
2022-11-10 19:59:31 -0500

    Add Part_I

---
+++ ASSIGNMENT_08/Part_I/4.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+	int arr[800];
+	int second[800] = {9};
+	printf("Hello World");
+	return 0;
+}
---
+++ ASSIGNMENT_08/Part_I/5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	int first[800];
+	int second[800] = {5};
+	printf("Hello World!");
+	return 0;
+}
+
